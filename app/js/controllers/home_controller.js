339cbf52127520400e4cc9ab6d0b1d3fd4d6f151
Tharshan <tharshan09@gmail.com>
2013-11-16 18:54:19 +0000

    final commit

--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -1,6 +1,6 @@
 angular.module("app").controller('HomeController', function($scope, $location, socket,$rootScope, $mobileFrame) {
   $scope.rooms = [];
-  $scope.chats = {'witlr':[{username:'test',message:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna'}], 'comsci':[], 'cardiffuni':[],'test':[]}
+  $scope.chats = {'witlr':[{username:'witler mod',message:'Welcome to HackChat you have joined witlr'}], 'comsci':[], 'cardiffuni':[],'test':[]}
   $scope.joined = [];
 
   $scope.login =  function(){
@@ -11,7 +11,7 @@ angular.module("app").controller('HomeController', function($scope, $location, s
   };
   $scope.send = function (room) {
     socket.emit('msg', {'room':room, 'msg':$scope.currentMsg, 'username':$rootScope.currentUser});
-    $scope.chats[room].push({'username':$rootScope.currentUser, 'message':$scope.currentMsg});
+    $scope.chats[room].push({'username':$rootScope.currentUser, 'message':$scope.currentMsg, 'class':'outbound'});
   };
   socket.on('onMsg', function(data){
     $scope.chats[data.room].push({'username':data.username, 'message':data.message});
@@ -25,6 +25,7 @@ angular.module("app").controller('HomeController', function($scope, $location, s
   $scope.goToRoom = function(room) {
     $location.path('/chat');
     // $scope.msgs = $scope.chats[room];
+    socket.emit('joinRoom', {'username':$rootScope.currentUser, 'room':room})
     $scope.joined.push(room);
     $scope.currentRoom = room;
   };

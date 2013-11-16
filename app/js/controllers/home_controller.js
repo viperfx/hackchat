14e72fc189fa7579cb070b135140b2e7e596e8ef
Tharshan <tharshan09@gmail.com>
2013-11-16 16:15:57 +0000

    sync changes

--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -1,12 +1,14 @@
-angular.module("app").controller('HomeController', function($scope, $location, socket) {
+angular.module("app").controller('HomeController', function($scope, $location, socket, $mobileFrame) {
   $scope.rooms = [];
 
   $scope.login =  function(username){
   	socket.emit('connect', username);
+
   }
 
   socket.on('onConnect', function(data) {
       console.log(data);
       $scope.rooms = data;
+
   });
 });

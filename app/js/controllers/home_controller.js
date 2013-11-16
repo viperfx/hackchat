69faca0b6ae49c5d11e80400c4b1ad2f0ead6e6a
Tharshan <tharshan09@gmail.com>
2013-11-16 16:03:36 +0000

    fucking angularjs!!!!

--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -1,17 +1,12 @@
 angular.module("app").controller('HomeController', function($scope, $location, socket) {
   $scope.rooms = [];
 
-<<<<<<< HEAD
   $scope.login =  function(username){
   	socket.emit('connect', username);
   }
 
-  	socket.on('onConnect', function(data) {
-=======
-    socket.emit('connect', 'Daniel');
   socket.on('onConnect', function(data) {
       console.log(data);
->>>>>>> 6d590fd491648a59950c0334a2cce445ff06e4fa
       $scope.rooms = data;
   });
 });
